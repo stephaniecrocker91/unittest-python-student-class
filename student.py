8c17cf62de6dad30a80f3b298fc81a4c74a826e7
Stephie <stephaniecrocker91@gmail.com>
2022-06-11 12:17:24 +0000

    Last test using mocking

--- student.py
+++ student.py
@@ -1,7 +1,9 @@
 from datetime import date, timedelta
+import requests
+
 
 class Student:
-    """Astudent class as base for method testing"""
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -14,11 +16,21 @@ class Student:
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
 
-    
-    def alert_santa(self):
-        self.naughty_list = True
-
-    
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
--- test_student.py
+++ test_student.py
@@ -1,20 +1,17 @@
 import unittest
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 class TestStudent(unittest.TestCase):
-
-
+    
     @classmethod
     def setUpClass(cls):
         print("set up class")
 
     def setUp(self):
-        print('setup')
-        # student is an instance variable 
-        # Since student is now an instance variable,  
-        # we’ll need to go and change every 
-        # reference from student to self.student.
-        student = Student('John', 'Doe')
+        print("setup")
+        self.student = Student("John", "Doe")
 
     @classmethod
     def tearDownClass(cls):
@@ -23,18 +20,44 @@ class TestStudent(unittest.TestCase):
     def tearDown(self):
         print("tear down")
 
-
     def test_full_name(self):
-        self.assertEqual(self.student.full_name, 'John Doe')
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        student.alert_santa()
+        print("test_alert_santa")
+        self.student.alert_santa()
         self.assertTrue(self.student.naughty_list)
 
-    
     def test_email(self):
+        print("test_email")
         self.assertEqual(self.student.email, "john.doe@email.com")
 
-# we can run the file  without having to specify the Unittest module.
-if __name__ == '__main__':    
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+        
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+            
+    def test_course_schedule_failed(self):
+        """
+        In the path "student" comes from the name of the file student.py
+        If you have named the file something else - it will need to match
+        eg, students.py would need "students.requests.get"
+        """
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong")
+            
+
+if __name__ == "__main__":
     unittest.main()
